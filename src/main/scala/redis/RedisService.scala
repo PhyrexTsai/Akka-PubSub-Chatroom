36dc0d7ce5bfbbfd40aab7aebb91eb282f7b2b8b
PhyrexTsai <x01ep23i@hotmail.com>
2016-03-06 00:46:35 +0800

    update redis

--- src/main/scala/redis/RedisService.scala
+++ src/main/scala/redis/RedisService.scala
@@ -1,15 +1,29 @@
 package redis
 
 import java.net.URI
-import redis.clients.jedis.Jedis
+import org.apache.commons.pool.impl.GenericObjectPool.Config
+import redis.clients.jedis.{Protocol, JedisPool, Jedis}
 
 /**
   * Created by Phyrex on 2016/3/5.
   */
 object RedisService {
-  private val redisURI : URI = new URI(System.getenv("REDIS_URL"));
-  private val jedis : Jedis = new Jedis(redisURI.getHost, redisURI.getPort)
+  private val redisURI : URI = new URI(System.getenv("REDIS_URL"))
+  private val redisPassword: Option[String] = Option(redisURI.getUserInfo).map(_.split(":").apply(1))
+  //lazy val redisPool: JedisPool = createRedisPool()
   def connection : Jedis = {
+    val jedis : Jedis = new Jedis(redisURI.getHost, redisURI.getPort)
+    for (p <- redisPassword) {
+      jedis.auth(p)
+    }
     jedis
   }
+  /*def createRedisPool(): JedisPool = {
+    val config = new Config()
+    config.testOnBorrow = true
+    redisPassword match {
+      case Some(password) => new JedisPool(config, redisURI.getHost, redisURI.getPort, Protocol.DEFAULT_TIMEOUT, password)
+      case None => new JedisPool(config, redisURI.getHost, redisURI.getPort, Protocol.DEFAULT_TIMEOUT, null)
+    }
+  }*/
 }
