b9dbe967c8ab1058a9f0c0e70c1275ea0519914a
PhyrexTsai <x01ep23i@hotmail.com>
2016-03-14 17:25:03 +0800

    modfiy

--- src/main/scala/chat/ChatHandler.scala
+++ src/main/scala/chat/ChatHandler.scala
@@ -17,40 +17,17 @@ object ChatHandler {
     new ChatHandler {
       // Flow => 1 input, 1 output   Source => 0 input, 1 output    Sink => 1 input, 0 output
       // 這邊使用 chatFlow 來表示使用者收到訊息、登入與登出的整個串流
-      /*def chatFlow(topic : String, sender : String) : Flow[String, Events.Message, Any] = {
-        val chatActor = system.actorOf(Props(classOf[ChatClient], topic))
-
-        def remove = {
-          Left(sender)
-        }
-
-        val in = Flow[String]
-          .map((msg) => {
-            ReceivedMessage(sender, msg)
-          })
-          .to(Sink.actorRef[ChatEvent](chatActor, remove))
-
-        val out = Source.actorRef[Events.Message](10, OverflowStrategy.fail)
-          .mapMaterializedValue((ref) => {
-            chatActor ! Join(sender, ref)
-          })
-
-        Flow.fromSinkAndSource(in, out)
-      }*/
+      // 練習將 chatFlow 使用 Flow.fromGraph 取代 Flow.fromSinkAndSource 進行改寫，Graph 可以更容易理解整個架構
+      // reference : http://doc.akka.io/docs/akka/2.4.2/scala/stream/stream-graphs.html
       def chatFlow(topic : String, sender : String) : Flow[String, Events.Message, Any] = {
         val chatActor = system.actorOf(Props(classOf[ChatClient], topic))
-
         Flow.fromGraph(GraphDSL.create() { implicit b =>
           val sink = b.add(Flow[String]
-            .map((msg) => {
-              ReceivedMessage(sender, msg)
-            })
+            .map(ReceivedMessage(sender, _))  // 這邊的 wildcard type 為 String
             .to(Sink.actorRef[ChatEvent](chatActor, Left(sender))))
 
-          val source = b.add(Source.actorRef[Events.Message](10, OverflowStrategy.fail)
-            .mapMaterializedValue((ref) => {
-              chatActor ! Join(sender, ref)
-            }))
+          val source = b.add(Source.actorRef[Events.Message](10, OverflowStrategy.fail) // 提高 buffer 的數量，可以避免發送大量訊息的時候容易死掉
+            .mapMaterializedValue(chatActor ! Join(sender, _))) // 這邊的 wildcard type 為 ActorRef
 
           FlowShape(sink.in, source.out)
         })
