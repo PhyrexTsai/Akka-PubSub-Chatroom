da13c39b72adc60bd5030c3d94d205d1c5bff183
PhyrexTsai <x01ep23i@hotmail.com>
2016-03-16 17:58:20 +0800

    add test

--- src/test/scala/ChatClientSpec.scala
+++ src/test/scala/ChatClientSpec.scala
@@ -14,6 +14,10 @@ import scala.concurrent.duration._
 class ChatClientSpec extends TestKit(ActorSystem("ActorSpec")) with DefaultTimeout with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
   val topic = "topic"
 
+  override def afterAll {
+    shutdown()
+  }
+
   "Mediator" should {
     "send Events.Joined to all subscribers" in {
       val mediator = DistributedPubSub(system).mediator
@@ -25,9 +29,37 @@ class ChatClientSpec extends TestKit(ActorSystem("ActorSpec")) with DefaultTimeo
       val client = system.actorOf(Props(classOf[ChatClient], topic), "client")
       client ! Join("client", client)
 
-      member1.expectMsgPF(3 seconds){case Events.Joined(sender, message) => {println(Events.Joined(sender, message));true}}
-      member2.expectMsgPF(3 seconds){case Events.Joined(sender, message) => {println(Events.Joined(sender, message));true}}
+      member1.expectMsgPF(3 seconds){case Events.Joined(sender, members) => {println(Events.Joined(sender, members));true}}
+      member2.expectMsgPF(3 seconds){case Events.Joined(sender, members) => {println(Events.Joined(sender, members));true}}
 
+    }
+
+    "send Events.ChatMessage to all subscribers" in {
+      val mediator = DistributedPubSub(system).mediator
+      val member1 = TestProbe()
+      val member2 = TestProbe()
+      mediator ! Subscribe(topic, member1.ref)
+      mediator ! Subscribe(topic, member2.ref)
+
+      val client2 = system.actorOf(Props(classOf[ChatClient], topic), "client2")
+      client2 ! ReceivedMessage("client2","hello")
+
+      member1.expectMsgPF(3 seconds){case msg : Events.ChatMessage => {println(msg);true}}
+      member2.expectMsgPF(3 seconds){case msg : Events.ChatMessage => {println(msg);true}}
+    }
+
+    "send Events.Leaved to all subscribers" in {
+      val mediator = DistributedPubSub(system).mediator
+      val member1 = TestProbe()
+      val member2 = TestProbe()
+      mediator ! Subscribe(topic, member1.ref)
+      mediator ! Subscribe(topic, member2.ref)
+
+      val client3 = system.actorOf(Props(classOf[ChatClient], topic), "client3")
+      client3 ! Left("client3")
+
+      member1.expectMsgPF(3 seconds){case msg : Events.ChatMessage => {println(msg);true}}
+      member2.expectMsgPF(3 seconds){case msg : Events.ChatMessage => {println(msg);true}}
     }
   }
 }
